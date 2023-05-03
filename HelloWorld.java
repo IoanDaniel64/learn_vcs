80376be8ce7eef621dd70d83109bb69a9a9dd0d6
Ioan Daniel <ioandaniel64@yahoo.com>
2023-05-03 18:05:37 +0300

    added helloe world program example

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,2 +1,9 @@
 //Hello Everyone
 //I am learning vcs
+
+public class HelloWorld {
+	public static void main (String[] args) {}
+		System.out.println("Hello World! We i am learning VCS. ") ;
+}
+}
+
